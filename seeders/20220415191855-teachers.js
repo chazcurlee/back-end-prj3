b5bfb28820f04c50c9db2a5339cc2ea55cbf1518
Ran Kim <rannio@gmail.com>
2022-04-15 16:16:30 -0400

    created seed data for teachers

--- seeders/20220415191855-teachers.js
+++ seeders/20220415191855-teachers.js
@@ -1,24 +1,45 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'teachers',
+      [
+        {
+          firstName: 'Bob',
+          lastName: 'Bobson',
+          username: 'bobbobson',
+          email: 'bobbobson@gmail.com',
+          password: 'bobbobson123',
+          experience: 'I have a lot of experience',
+          about: 'My name is Bob',
+          image: ''
+        },
+        {
+          firstName: 'Will',
+          lastName: 'Willson',
+          username: 'WillWillson',
+          email: 'WillWillson@gmail.com',
+          password: 'WillWillson123',
+          experience: 'I have a lot of experience',
+          about: 'My name is Will',
+          image: ''
+        },
+        {
+          firstName: 'Sam',
+          lastName: 'Samson',
+          username: 'SamSamson',
+          email: 'SamSamson@gmail.com',
+          password: 'SamSamson123',
+          experience: 'I have a lot of experience',
+          about: 'My name is Sam',
+          image: ''
+        },
+      ]
+    )
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('teachers', null, {})
   }
 };
