640ab83d9733f11b06a306be101b59f37b1a3810
Ran Kim <rannio@gmail.com>
2022-04-18 11:38:32 -0400

    changed seed data

--- seeders/20220418152322-comments.js
+++ seeders/20220418152322-comments.js
@@ -6,22 +6,22 @@ module.exports = {
       'comments',
       [
         {
-          post_id: 9,
+          post_id: 1,
           name: 'Claire',
           content: 'I love this yoga pose!'
         },
         {
-          post_id: 10,
+          post_id: 2,
           name: 'Harry',
           content: 'Thank you for sharing this!'
         },
         {
-          post_id: 11,
+          post_id: 3,
           name: 'Mark',
           content: 'Such an intense workout!'
         },
         {
-          post_id: 11,
+          post_id: 3,
           name: 'Emma',
           content: 'I was sooooo sore the day after!'
         },
