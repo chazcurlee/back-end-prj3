d06ef2a610803dc25640dc2c68418b063a688d17
chazcurlee <chaz.curlee@gmail.com>
2022-05-24 11:50:54 -0400

    cors options

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
+app.options('*')
 
 /////////// DEFINE CONTROLLER VARIABLES /////////////
 const controllerT = require('./controllers/TeacherController')
